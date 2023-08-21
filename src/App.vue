3026bf8b6389033bba5ebc3179081b3451e633bc
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2023-08-21 16:02:54 +0200

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+
+
+  </div>
+</template>
+
+<style scoped>
+@media (prefers-reduced-motion: reduce) {
+
+  * {
+    transition: none !important;
+  }
+}
+</style>
