31287aa97b9fb66edc77c340e497d10d10306df2
WenzelburgerMarc <54157548+WenzelburgerMarc@users.noreply.github.com>
2023-08-22 12:50:48 +0200

    Finished

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@ import TheAccordion from './components/Accordion/TheAccordion.vue';
 </script>
 
 <template>
-  <div>
+  <div class="overflow-hidden">
     <TheSidebar />
 
-    <div class="main-content-container  w-[calc(100vw-70px)] min-h-screen flex justify-center items-center p-5">
-
-      <the-accordion />
+    <div class="main-content-container w-[calc(100vw-70px)] min-h-screen flex flex-col justify-center items-center p-5">
+      <div class="demoTitle mb-5 mt-14">
+        <h1 class="text-5xl font-gray-800 font-light">Sidebar + Accordion Demo</h1>
+      </div>
+      <the-accordion class="mb-auto" />
     </div>
 
   </div>
@@ -18,14 +20,13 @@ import TheAccordion from './components/Accordion/TheAccordion.vue';
 
 <style scoped>
 .main-content-container {
-  overflow-x: hidden;
-  margin-left: 70px;
+  overflow-x: hidden !important;
+  transform: translateX(70px);
   transition: all 0.45s ease-in-out;
 }
 
 .translate-blur {
-  margin-left: inherit;
-
+  overflow: hidden;
   transform: translateX(224px) scale(0.8);
   filter: blur(5px);
 }
